2d077adb60b5e0aaf15d4de31d132a8d5ce7aca7
baeksunghyun <ces0135@naver.com>
2025-01-02 00:32:06 +0900

    메시지 JSON으로 직접 입력받기

--- chat/src/main/java/com/ll/chat/ChatController.java
+++ chat/src/main/java/com/ll/chat/ChatController.java
@@ -1,13 +1,11 @@
 package com.ll.chat;
 
 import com.ll.chat.dto.MessageResponse;
+import com.ll.chat.dto.WriteMessageRequest;
 import com.ll.chat.dto.WriteMessageResponse;
 import lombok.Getter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +17,17 @@ public class ChatController {
 
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<WriteMessageResponse> writeMessage() {
+    public RsData<WriteMessageResponse> writeMessage(@RequestBody WriteMessageRequest writeMessageRequest) {
+        /*
         ChatMessage ch = new ChatMessage("홍길동", "안녕하세요.");
         chatMessages.add(ch);
-
         ChatMessage ch2 = new ChatMessage("김철수", "안녕하세요.");
         chatMessages.add(ch2);
+        */
+        // 직접 입력 받는 방식으로 변경
+        ChatMessage cm = new ChatMessage(writeMessageRequest.getAuthorName(), writeMessageRequest.getContent());
+
+        chatMessages.add(cm);
 
         return new RsData("200", "메세지가 작성되었습니다.", new WriteMessageResponse(chatMessages));
     }
--- chat/src/main/java/com/ll/chat/dto/WriteMessageRequest.java
+++ chat/src/main/java/com/ll/chat/dto/WriteMessageRequest.java
@@ -1,5 +1,10 @@
 package com.ll.chat.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class WriteMessageRequest {
     private String authorName;
     private String content;
