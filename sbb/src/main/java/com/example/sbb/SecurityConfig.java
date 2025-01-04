2ae972fd4ca278a4201b0cb51e49fca4900133eb
baeksunghyun <ces0135@naver.com>
2025-01-04 14:49:13 +0900

    Spring Security 설정'

---
+++ sbb/src/main/java/com/example/sbb/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+// @Configuration은 이 파일이 스프링의 환경 설정 파일임을 의미하는 애너테이션이다.
+@Configuration
+// @EnableWebSecurity는 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애너테이션이다.
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())  // 인증되지 않은 모든 페이지의 요청을 허락한다는 의미
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))  // h2-console 페이지의 요청은 CSRF 보호를 무시한다는 의미
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))) // h2-console 페이지 UI 깨짐 방지
+        ;
+        return http.build();
+    }
+}
