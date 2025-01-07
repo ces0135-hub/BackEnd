e33295d14d9ab507d8aa91b60c3aa111012c1c65
baeksunghyun <ces0135@naver.com>
2025-01-07 12:00:58 +0900

    Thymeleaf를 통해서 html 불러오기 및 RequestMapping()으로 url 묶기

--- mulitChat/src/main/java/com/ll/mulitChat/domain/chat/ChatRoom/controller/ChatRoomController.java
+++ mulitChat/src/main/java/com/ll/mulitChat/domain/chat/ChatRoom/controller/ChatRoomController.java
@@ -3,23 +3,27 @@ package com.ll.mulitChat.domain.chat.ChatRoom.controller;
 import com.ll.mulitChat.domain.chat.ChatRoom.service.ChatRoomService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("/chat/room")
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
 
-    @GetMapping("/chat/room/{roomId}")
-    @ResponseBody
+    @GetMapping("/{roomId}")
+    @ResponseBody // JSON 형태로 문자열을 보낼 때 사용
     public String showRoom(@PathVariable long roomId, @RequestParam(defaultValue = "NoName") String writerName) {
         // RequestParam은 url 뒤에 ?로 오는 부분
         // default 값으로 writerName이 없을 때 출력되는 내용 설정
 
         return String.format("%d번 채팅방입니다. writer: %s", roomId, writerName);
+    }
+
+    @GetMapping("/make")
+    public String makeRoom() {
+        // Thymeleaf로 html 파일을 불러옴
+        return "domain/chat/chatRoom/make";
     }
 }
