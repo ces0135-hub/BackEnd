3217d94a63cde638357883de3dff317abb52fcd6
baeksunghyun <ces0135@naver.com>
2025-01-07 14:37:10 +0900

    IntStream으로 채팅 data 넣기

--- mulitChat/src/main/java/com/ll/mulitChat/domain/chat/ChatMessage/service/ChatMessageService.java
+++ mulitChat/src/main/java/com/ll/mulitChat/domain/chat/ChatMessage/service/ChatMessageService.java
@@ -1,6 +1,8 @@
 package com.ll.mulitChat.domain.chat.ChatMessage.service;
 
+import com.ll.mulitChat.domain.chat.ChatMessage.entity.ChatMessage;
 import com.ll.mulitChat.domain.chat.ChatMessage.repository.ChatMessageRepository;
+import com.ll.mulitChat.domain.chat.ChatRoom.entity.ChatRoom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ChatMessageService {
     private final ChatMessageRepository chatMessageRepository;
+
+    public void write(ChatRoom chatRoom, String writerName, String content) {
+        // 채팅 내용 생성
+        ChatMessage chatMessage = ChatMessage.builder()
+                .chatRoom(chatRoom)
+                .writerName(writerName)
+                .content(content)
+                .build();
+
+        // 채팅 내용 넣기
+        chatMessageRepository.save(chatMessage);
+    }
 }
--- mulitChat/src/main/java/com/ll/mulitChat/global/initData/NotProd.java
+++ mulitChat/src/main/java/com/ll/mulitChat/global/initData/NotProd.java
@@ -1,5 +1,6 @@
 package com.ll.mulitChat.global.initData;
 
+import com.ll.mulitChat.domain.chat.ChatMessage.service.ChatMessageService;
 import com.ll.mulitChat.domain.chat.ChatRoom.entity.ChatRoom;
 import com.ll.mulitChat.domain.chat.ChatRoom.service.ChatRoomService;
 import org.springframework.boot.ApplicationRunner;
@@ -7,17 +8,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.stream.IntStream;
+
 @Configuration
 @Profile("!prod") // 배포 환경이 아닐 때 실행됨
 public class NotProd {
     @Bean
-    public ApplicationRunner initNotProd(ChatRoomService chatRoomService) {
+    public ApplicationRunner initNotProd(ChatRoomService chatRoomService, ChatMessageService chatMessageService) {
         return args -> {
             ChatRoom chatRoom1 = chatRoomService.make("공부");
             ChatRoom chatRoom2 = chatRoomService.make("식사");
             ChatRoom chatRoom3 = chatRoomService.make("휴식");
-//            IntStream.rangeClosed(1, 100).forEach(num -> {
-//            });
+
+            // 채팅 데이터 넣기
+            IntStream.rangeClosed(1, 100).forEach(num -> {
+                chatMessageService.write(chatRoom1, "홍길동", "공부 메시지" + num);
+            });
             System.out.println("Not Prod");
         };
     }
