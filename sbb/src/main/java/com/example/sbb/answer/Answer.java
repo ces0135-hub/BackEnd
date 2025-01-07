c999632a2be23d620330efe45c6804b090f02626
baeksunghyun <ces0135@naver.com>
2025-01-07 10:02:15 +0900

    chat_app 웹소켓 방식으로 구현

--- sbb/src/main/java/com/example/sbb/answer/Answer.java
+++ sbb/src/main/java/com/example/sbb/answer/Answer.java
@@ -5,7 +5,6 @@ import com.example.sbb.user.SiteUser;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Set;
 
