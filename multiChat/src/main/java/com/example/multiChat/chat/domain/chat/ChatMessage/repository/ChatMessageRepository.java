aba5b9491951995943ec623451123d49b1e4588a
baeksunghyun <ces0135@naver.com>
2025-01-08 21:02:52 +0900

    ChatApp Using OpenAI init

--- multiChat/src/main/java/com/example/multiChat/chat/domain/chat/ChatMessage/repository/ChatMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.multiChat.chat.domain.chat.ChatMessage.repository;
-
-import com.example.multiChat.chat.domain.chat.ChatMessage.entity.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-}
--- multiChat/src/main/java/com/example/multiChat/chat/domain/chat/ChatRoom/repository/ChatRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.multiChat.chat.domain.chat.ChatRoom.repository;
-
-import com.example.multiChat.chat.domain.chat.ChatRoom.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-}
