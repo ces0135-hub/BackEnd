8885928bdbed83ab49ccc78a9e54da19dfe3ad3a
baeksunghyun <ces0135@naver.com>
2025-01-10 15:18:49 +0900

    게시글 삭제 메서드, @OneToMany, @ManytoOne에 fetch = FetchType.LAZY 적용

--- chat-app-mession-main/src/main/java/com/ll/chatApp/domain/article/article/entity/Article.java
+++ chat-app-mession-main/src/main/java/com/ll/chatApp/domain/article/article/entity/Article.java
@@ -4,6 +4,7 @@ import com.ll.chatApp.domain.article.article.articleComment.entity.ArticleCommen
 import com.ll.chatApp.domain.member.member.entity.Member;
 import com.ll.chatApp.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.*;
@@ -25,12 +26,15 @@ public class Article extends BaseEntity {
     private String title;
     private String content;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member author;
 
-    @OneToMany(mappedBy = "article", cascade = ALL)
+    // @OneToMany의 기본 설정
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "article", cascade = ALL, orphanRemoval = true)
+    // orphanRemoval = true: 자식 요소를 추적해서 삭제해줌
     @Builder.Default  // 안 붙여주면 comments가 null 값으로 초기화됨
     private List<ArticleComment> comments = new ArrayList<>();
+
 
     public void addComment(Member memberAuthor, String commentBody) {
         ArticleComment articleComment = ArticleComment.builder()
--- chat-app-mession-main/src/main/java/com/ll/chatApp/domain/article/article/service/ArticleService.java
+++ chat-app-mession-main/src/main/java/com/ll/chatApp/domain/article/article/service/ArticleService.java
@@ -9,6 +9,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -52,5 +53,9 @@ public class ArticleService {
     @Transactional  //.save()가 필요없음
     public void modifyComment(ArticleComment comment, String commentBody) {
         comment.setBody(commentBody);
+    }
+
+    public List<Article> findAll() {
+        return articleRepository.findAll();
     }
 }
