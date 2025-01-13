9266797593a96382ae40265c179a68f4ea743c0e
baeksunghyun <ces0135@naver.com>
2025-01-13 19:15:21 +0900

    검색 기능 init

--- chat-app-mession-main/src/main/java/com/ll/chatApp/global/initData/NotProd.java
+++ chat-app-mession-main/src/main/java/com/ll/chatApp/global/initData/NotProd.java
@@ -65,10 +65,6 @@ public class NotProd {
                 article3.addComment(member1, "댓글11");
                 article3.addComment(member1, "댓글12");
 
-//                article1.addTag("자바");
-//                article1.addTag("스프링");
-//                article2.addTag("데이터베이스");
-//                article2.addTag("JPA");
                 article1.addTag("자바");
                 article1.addTag("백엔드");
                 article2.addTags("프레임워크", "스프링부트");
