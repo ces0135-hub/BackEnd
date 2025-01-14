0c4950a7cae2c046c6a418cb37e120b5b05bf005
baeksunghyun <ces0135@naver.com>
2025-01-14 10:06:52 +0900

    QuderyDSL 세팅 init

--- chat-app-mession-main/src/main/java/com/ll/chatApp/domain/article/article/articlecController/ArticleController.java
+++ chat-app-mession-main/src/main/java/com/ll/chatApp/domain/article/article/articlecController/ArticleController.java
@@ -2,7 +2,6 @@ package com.ll.chatApp.domain.article.article.articlecController;
 
 import com.ll.chatApp.domain.article.article.entity.Article;
 import com.ll.chatApp.domain.article.article.service.ArticleService;
-import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,7 +21,6 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/article")
 @RequiredArgsConstructor
-@Slf4j
 public class ArticleController {
     private final ArticleService articleService;
 
